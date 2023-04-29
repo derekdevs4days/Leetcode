4f6d0914b4b46ee2280e8f9de492cffb1c8952a3
Derek Truong <derektruongdev@gmail.com>
2023-04-29 10:14:38 -0500

    not complete, neet to add questions

---
+++ easy/reverse_linked_list.js
@@ -0,0 +1,12 @@
+const reverseList = function(head) {
+  let prev = null;
+  
+   while (head) {
+    let temp = head.next;
+    head.next = prev;
+    prev = head;
+    head = temp;
+  }
+
+  return prev;
+}
